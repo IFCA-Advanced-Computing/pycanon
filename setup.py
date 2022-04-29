8c0e47b5149414c6e9fa2e38bedfc2ffcf24e5f6
Judith Sainz-Pardo Diaz <judithspd2@gmail.com>
2022-04-29 13:23:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name = 'check-anonymity',
+    name = 'check_anonymity',
     version = '0.0.1',
     author = 'Judith sáinz-Pardo Díaz',
     author_email = 'sainzpardo@ifca.unican.es',
