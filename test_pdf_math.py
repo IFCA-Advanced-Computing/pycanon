5f50f290fcad0985b9706fdb7c94ca2ae243f212
Judith Sainz-Pardo Diaz <judithspd2@gmail.com>
2022-05-11 13:58:56 +0200

    Update example of report generation

--- test_pdf_math.py
+++ test_pdf_math.py
@@ -2,7 +2,12 @@
 from pycanon import test_anonymity
 
 QI = ['Teacher', 'Gender', 'Ethnic', 'Freeredu', 'wesson']
-FILE_NAME = './Data/Processed/StudentsMath_Score.csv'
+FILE_NAME_1 = './Data/Processed/StudentsMath_Score.csv'
 SA = ['Score']
-FILE_PDF = 'test_math.pdf'
-test_anonymity.get_anon_report(FILE_NAME, QI, SA, file_pdf = FILE_PDF)
+FILE_PDF_1 = 'test_math.pdf'
+test_anonymity.get_anon_report(FILE_NAME_1, QI, SA, file_pdf = FILE_PDF_1)
+
+FILE_NAME_2 = './Data/Processed/StudentsMath_Score_k5.csv'
+FILE_PDF_2 = 'test_math_k5.pdf'
+test_anonymity.get_anon_report(FILE_NAME_2, QI, SA, file_pdf = FILE_PDF_2)
+
