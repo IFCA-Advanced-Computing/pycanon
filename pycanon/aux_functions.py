2c1e53a8afb0c5286946e3921c926aa52fb62f4d
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2022-05-13 12:34:31 +0200

    Move report generation into its own module

--- pycanon/aux_functions.py
+++ pycanon/aux_functions.py
@@ -1,6 +1,26 @@
-"""Module with different functions which calculate properties about anonymity:
-k-anonymity, (alpha,k)-anonymity, l-diversity, entropy l-diversity, (c,l)-diversity,
-basic beta-likeness, enhanced beta-likeness, t-closeness and delta-disclosure privacy."""
+# -*- coding: utf-8 -*-
+
+# Copyright 2022 Spanish National Research Council (CSIC)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Module with different functions which calculate properties about anonymity.
+
+k-anonymity, (alpha,k)-anonymity, l-diversity, entropy l-diversity,
+(c,l)-diversity, basic beta-likeness, enhanced beta-likeness, t-closeness and
+delta-disclosure privacy.
+"""
 
 import os
 import numpy as np
