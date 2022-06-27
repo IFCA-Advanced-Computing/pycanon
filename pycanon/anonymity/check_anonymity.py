2b1a6a507c9923fb9a398a91ffcb6550568143d9
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2022-06-27 11:10:26 +0200

    Move (alpha,k)-anonimity into its own module

--- pycanon/anonymity/check_anonymity.py
+++ pycanon/anonymity/check_anonymity.py
@@ -73,65 +73,6 @@ def achieve_l_diversity(data: pd.DataFrame,
     return data_new
 
 
-def calculate_alpha_k(data: pd.DataFrame,
-                      quasi_ident: typing.List,
-                      sens_att: typing.List,
-                      gen=True) -> typing.Tuple[float, int]:
-    """Calculate alpha and k for (alpha,k)-anonymity.
-
-    :param data: dataframe with the data under study.
-    :type data: pandas dataframe
-
-    :param quasi_ident: list with the name of the columns of the dataframe
-        that are quasi-identifiers.
-    :type quasi_ident: list of strings
-
-    :param sens_att: list with the name of the columns of the dataframe
-        that are the sensitive attributes.
-    :type sens_att: list of strings
-
-    :param gen: boolean, default to True. If true, it is generalized for the
-        case of multiple SA, if False, the set of QI is updated for each SA
-    :type  gen: boolean
-
-    :return: alpha and k values for (alpha,k)-anonymity.
-    :rtype: alpha is a float, k is an int.
-    """
-    quasi_ident = np.array(quasi_ident)
-    sens_att = np.array(sens_att)
-    utils.check_qi(data, quasi_ident)
-    utils.check_sa(data, sens_att)
-    k_anon = calculate_k(data, quasi_ident)
-    equiv_class = utils.get_equiv_class(data, quasi_ident)
-    if gen:
-        alpha_ec = []
-        for ec in equiv_class:
-            data_temp = data.iloc[utils.convert(ec)]
-            alpha_sa = []
-            for sa in sens_att:
-                values = np.unique(data_temp[sa].values)
-                _alpha = [len(data_temp[data_temp[sa] == s]) /
-                          len(data_temp) for s in values]
-                alpha_sa.append(max(_alpha))
-            alpha_ec.append(max(alpha_sa))
-        alpha = max(alpha_ec)
-    else:
-        alpha_sa = []
-        for i, sa in enumerate(sens_att):
-            tmp_qi = np.concatenate([quasi_ident, np.delete(sens_att, i)])
-            equiv_class = utils.get_equiv_class(data, tmp_qi)
-            alpha_ec = []
-            for ec in equiv_class:
-                data_temp = data.iloc[utils.convert(ec)]
-                values = np.unique(data_temp[sa].values)
-                _alpha = [len(data_temp[data_temp[sa] == s]) /
-                          len(data_temp) for s in values]
-                alpha_ec.append(max(_alpha))
-            alpha_sa.append(max(alpha_ec))
-        alpha = max(alpha_sa)
-    return alpha, k_anon
-
-
 def calculate_basic_beta(data: pd.DataFrame,
                          quasi_ident: typing.List,
                          sens_att: typing.List,
