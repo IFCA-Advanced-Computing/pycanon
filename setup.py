a42678a2174c493580849ec0bc51fd569c2bf046
Judith Sainz-Pardo Diaz <judithspd2@gmail.com>
2023-04-03 10:28:31 +0200

    Reformat files using black

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 # Passing requirements as file only works on setuptools >= 62.6
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
