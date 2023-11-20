926b7eb9eaa720dba35add2e87f7d3e448f6af61
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2023-11-21 00:00:22 +0100

    Move to pyproject.toml for project definition

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-# Passing requirements as file only works on setuptools >= 62.6
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-setuptools.setup(
-    install_requires=requirements,
-)
