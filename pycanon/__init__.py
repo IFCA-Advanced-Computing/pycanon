eecf7fb670ce71f7fca3a037dd6566d7d946198c
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2025-01-14 11:07:39 +0100

    fix: release 1.0.3

--- pycanon/__init__.py
+++ pycanon/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2022 Spanish National Research Council (CSIC)
+# Copyright 2025 Spanish National Research Council (CSIC)
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
