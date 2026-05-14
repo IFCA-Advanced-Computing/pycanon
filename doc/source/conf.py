c187c64a4cd23d558915505666b096421b61ceb6
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2026-05-14 10:05:21 +0200

    fix: move to 1.3.4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2025, Spanish National Research Council (CSIC)"
 author = "Spanish National Research Council (CSIC)"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.3"
+release = "1.3.4"
 
 
 # -- General configuration ---------------------------------------------------
